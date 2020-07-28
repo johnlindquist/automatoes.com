152fc611aa5e33a3a5e370d0ae1037e802b88b64
johnlindquist <johnlindquist@gmail.com>
2020-07-28 10:55:41 -0600

    🤠

---
+++ _layouts/gatsby-browser.js
@@ -0,0 +1,1 @@
+require("prismjs/themes/prism-coy.css")
--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -4,9 +4,34 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          `gatsby-remark-prismjs`,
+          "gatsby-remark-double-brackets-link",
+          "gatsby-remark-double-parenthesis-link",
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 561,
+            },
+          },
+          `gatsby-remark-copy-linked-files`,
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              icon: false,
+            },
+          },
+        ],
+      },
+    },
+    {
+      mdxOtherwiseConfigured: true,
       resolve: `gatsby-theme-garden`,
       options: {
-        rootNote: "/readme",
+        rootNote: "/index",
         contentPath: `${__dirname}/..`,
         ignore: [
           "**/_layouts/**",
