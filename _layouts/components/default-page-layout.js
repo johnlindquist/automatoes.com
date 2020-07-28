dd6b4d43819533fae08249689368426658fbecca
johnlindquist <johnlindquist@gmail.com>
2020-07-27 21:25:38 -0600

    🤠

---
+++ _layouts/components/default-page-layout.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Layout({ children }) {
+  console.log("HELLLOOOOOO")
+
+  return (
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 650,
+        padding: `0 1rem`,
+      }}
+    >
+      <h2>AYWOUFNTOAYUWNFTA</h2>
+      {children}
+    </div>
+  )
+}
