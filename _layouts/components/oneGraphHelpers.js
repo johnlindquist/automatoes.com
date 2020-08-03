35216502597afe92ebe1178174873537a904a8d4
Sean Grove <sean@bushi.do>
2020-08-03 11:48:04 -0700

    Update prettier to match existing code

--- _layouts/components/oneGraphHelpers.js
+++ _layouts/components/oneGraphHelpers.js
@@ -1,10 +1,10 @@
-import OneGraphAuth from "onegraph-auth";
+import OneGraphAuth from "onegraph-auth"
 
-export const ONE_GRAPH_APP_ID = "73a2441e-9638-496b-bf0b-b26c8c66720b";
-let isSsr = false;
+export const ONE_GRAPH_APP_ID = "73a2441e-9638-496b-bf0b-b26c8c66720b"
+let isSsr = false
 
 if (typeof window === "undefined") {
-  isSsr = true;
+  isSsr = true
 }
 
 export const auth = isSsr
@@ -15,24 +15,24 @@ export const auth = isSsr
   : new OneGraphAuth({
       appId: ONE_GRAPH_APP_ID,
       oneGraphOrigin: "https://serve.onegraph.com",
-    });
+    })
 
-export const currentUser = (auth) => {
-  if (isSsr) return { loading: true, user: null, error: null };
-  const accessToken = auth.accessToken();
+export const currentUser = auth => {
+  if (isSsr) return { loading: true, user: null, error: null }
+  const accessToken = auth.accessToken()
 
-  let decoded = null;
-  let error = null;
+  let decoded = null
+  let error = null
 
   if (!!accessToken) {
     try {
-      const payload = atob(accessToken.accessToken.split(".")[1]);
-      decoded = JSON.parse(payload);
-      delete decoded["https://onegraph.com/jwt/claims"];
+      const payload = atob(accessToken.accessToken.split(".")[1])
+      decoded = JSON.parse(payload)
+      delete decoded["https://onegraph.com/jwt/claims"]
     } catch (e) {
-      console.warn(`Error decoding OneGraph jwt for appId=${auth.appId}: `, e);
+      console.warn(`Error decoding OneGraph jwt for appId=${auth.appId}: `, e)
     }
   }
 
-  return { user: decoded, error: error, loading: false };
-};
+  return { user: decoded, error: error, loading: false }
+}
