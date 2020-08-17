b767179750ab2da829eaeae1c3d4a52c1ab398dc
johnlindquist <johnlindquist@gmail.com>
2020-08-17 14:59:30 -0600

    🤠

--- _layouts/components/NewRecipe.js
+++ _layouts/components/NewRecipe.js
@@ -1,9 +1,16 @@
-import React from "react"
+import React, { useContext } from "react"
 import { useFormik } from "formik"
+import { AuthProvider, AuthContext } from "react-onegraph"
+
+const APP_ID = "961b2d5a-83e1-4835-b5b9-c10e983efde6"
 
 const SignupForm = () => {
   // Pass the useFormik() hook initial form values and a submit function that will
   // be called when the form is submitted
+  const { login } = useContext(AuthContext)
+
+  login("github")
+
   const formik = useFormik({
     initialValues: {
       email: "",
